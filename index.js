90a64a561fc602178d36f0cd218ffcb569d3fa5b
ShrishtiAgarwal <shrishtiagarwal21@gmail.com>
2022-08-29 15:48:21 +0530

    first prachi-bhen mission of life

---
+++ index.js
@@ -0,0 +1,3 @@
+import { Prachi } from "./prachi.js";
+const prachi = new Prachi()
+console.log(prachi.getPrachisMissionOfLife())
---
+++ prachi.js
@@ -0,0 +1,10 @@
+export class Prachi {
+  constructor() {
+    this.intro =
+      "hey I am Prachi Suryawanshi, leading the quality product of bhen-lang";
+  }
+
+  getPrachisMissionOfLife = () => {
+    return this.intro;
+  };
+}
